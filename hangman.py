7d225173a32496bc6bfe7b8c40caed58e0e590f2
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-08-15 14:10:43 +0530

    defined hangman function

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 import random
 from words import words
+import string
 
 
 def get_valid_word(words):
@@ -7,4 +8,11 @@ def get_valid_word(words):
     while '-' in word or ' ' in word:
         # If there is a space or - in the word, we select another word randomly from the list
         word = random.choice(words)
-    return word
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word to be guessed
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # letters the user has already guessed
