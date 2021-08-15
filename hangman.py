84665afed7c7b9c88d8d4cc4fb8cf4c0fee2faaa
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-08-15 13:51:25 +0530

    defined get_valid_word function

--- hangman.py
+++ hangman.py
@@ -1,3 +1,10 @@
 import random
 from words import words
-print(words)
+
+
+def get_valid_word(words):
+    word = random.choice(words)  # randomly choose a word from the list
+    while '-' in word or ' ' in word:
+        # If there is a space or - in the word, we select another word randomly from the list
+        word = random.choice(words)
+    return word
