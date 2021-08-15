3a90cf85b4b4581a35dfcb6ac04667aeb0c45478
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-08-15 19:07:39 +0530

    completed the game without visual

--- hangman.py
+++ hangman.py
@@ -16,12 +16,13 @@ def hangman():
     word_letters = set(word)  # letters in the word to be guessed
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # letters the user has already guessed
+    lives = 6 #TODO: Cross check value
 
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
 
         # letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have ',lives,' lives left ','You have used these letters: ', ' '.join(used_letters))
 
         # what current word is (i.e. W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -34,12 +35,21 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            
+            else:
+                lives = lives - 1 # takes away a life if the letter guessed is not in the word
 
         elif user_letter in used_letters:
             print('You have already used that character. Please try again')
 
         else:
             print('Invalid character. Please try again')
+
+    if lives == 0:
+        print('Sorry you died, the word was ', word)
+
+    else:
+        print('Congratulations! You guessed the word ',word,' correctly!!!')
 
 if __name__ == '__main__':
     hangman()
